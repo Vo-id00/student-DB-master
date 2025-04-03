c9575e45d833ab23d13c8df7aa425ca96e964ea3
vignesh <vigneshjogi12@gmail.com>
2025-04-03 14:47:42 +0530

    css updated

--- src/app/components/InputField.js
+++ src/app/components/InputField.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function InputField({value,onChange,type,placeholder}){
     return (
-        <div className="bg-gray-200 p-4 rounded-md shadow-md w-[480px]">
+        <div className="bg-pink-200 m-2 border p-4 rounded-md shadow-md w-[480px]">
             <input type={type}
-            className="w-full p-3"
+            className="w-full p-3 bg-white"
             value={value}
             onChange={onChange} placeholder={placeholder}
             />
